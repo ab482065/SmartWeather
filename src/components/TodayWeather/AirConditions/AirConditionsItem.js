4829f090d189ffb179db1469d7dbf3ece4ca1b2d
ab482065 <108334048+ab482065@users.noreply.github.com>
2024-05-18 16:56:27 +0500

    Update AirConditionsItem.js

--- src/components/TodayWeather/AirConditions/AirConditionsItem.js
+++ src/components/TodayWeather/AirConditions/AirConditionsItem.js
@@ -1,94 +1,63 @@
-import { Box, Grid, SvgIcon } from '@mui/material';
-import React from 'react';
-import ThermostatIcon from '@mui/icons-material/Thermostat';
-import AirIcon from '@mui/icons-material/Air';
-import FilterDramaIcon from '@mui/icons-material/FilterDrama';
-import { ReactComponent as HumidityIcon } from '../../../assets/humidity.svg';
+import React, { useState } from 'react';
+import ErrorBox from '../../Reusable/ErrorBox';
+import AirConditionsItem from './AirConditionsItem';
+import Layout from '../../Reusable/Layout';
 
-const AirConditionsItem = (props) => {
-  let iconContent;
+const TodayWeatherAirConditions = ({ data }) => {
+  const [isValid, setIsValid] = useState(null);
 
-  if (props.type === 'temperature')
-    iconContent = <ThermostatIcon sx={{ fontSize: '18px' }} />;
-  else if (props.type === 'wind')
-    iconContent = <AirIcon sx={{ fontSize: '18px' }} />;
-  else if (props.type === 'clouds')
-    iconContent = <FilterDramaIcon sx={{ fontSize: '18px' }} />;
-  else if (props.type === 'humidity')
-    iconContent = (
-      <SvgIcon
-        component={HumidityIcon}
-        inheritViewBox
-        sx={{ fontSize: '18px' }}
-      />
+  const noDataProvided = !data || Object.keys(data).length === 0 || data.cod === '404';
+
+  let content = <ErrorBox flex="1" type="error" />;
+
+  if (!noDataProvided) {
+    setIsValid(true);
+    content = (
+      <>
+        <AirConditionsItem
+          title="Real Feel"
+          value={`${Math.round(data.main.feels_like)} °C`}
+          type="temperature"
+        />
+        <AirConditionsItem
+          title="Wind"
+          value={`${data.wind.speed} m/s`}
+          type="wind"
+        />
+        <AirConditionsItem
+          title="Clouds"
+          value={`${Math.round(data.clouds.all)} %`}
+          type="clouds"
+        />
+        <AirConditionsItem
+          title="Humidity"
+          value={`${Math.round(data.main.humidity)} %`}
+          type="humidity"
+        />
+      </>
     );
+  } else {
+    setIsValid(false);
+  }
 
   return (
-    <Grid
-      item
-      xs={3}
-      sx={{
-        padding: '0',
-        height: '80px',
-      }}
+    <Layout
+      title="AIR CONDITIONS"
+      content={content}
+      mb="1rem"
+      sx={{ marginTop: '2.9rem' }}
     >
-      <Grid
-        item
-        xs={12}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        sx={{
-          width: '100%',
-          height: '40px',
-          flexDirection: { xs: 'column', sm: 'row' },
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            color: 'rgba(255, 255, 255, .7)',
-            padding: 0,
-          }}
-        >
-          {iconContent}
-        </Box>
-        <Box
-          sx={{
-            color: 'rgba(255, 255, 255, .7)',
-            fontSize: { xs: '10px', sm: '12px', md: '14px' },
-            paddingLeft: { xs: '0px', sm: '4px', md: '6px' },
-            paddingTop: { xs: '2px', sm: '0px' },
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          {props.title}
-        </Box>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ height: '40px' }}
-      >
-        <Box
-          sx={{
-            fontFamily: 'Poppins',
-            fontWeight: '600',
-            fontSize: { xs: '12px', sm: '14px', md: '16px' },
-            color: 'white',
-            lineHeight: 1,
-          }}
-        >
-          {props.value}
-        </Box>
-      </Grid>
-    </Grid>
+      {isValid ? (
+        <p>
+          Flood indicator: <span style={{ color: 'green' }}>YES</span>
+        </p>
+      ) : (
+        <p>
+          Flood indicator: <span style={{ color: 'red' }}>NO</span>
+        </p>
+      )}
+    </Layout>
   );
 };
 
-export default AirConditionsItem;
+export default TodayWeatherAirConditions;
