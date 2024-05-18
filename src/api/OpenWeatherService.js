d5259c7153dc6de6d80595fd8b5353d1e5750d5c
ab482065 <108334048+ab482065@users.noreply.github.com>
2024-05-18 13:08:20 +0500

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '1369d21919f4fd9e0b40f5e510f6e26c';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
